1d1bb44685518e9bcff482aa42715fa530f70c15
Victor Redondo <vrviktor@gmail.com>
2022-10-06 23:19:37 +0200

    Add property length

--- public/js/SkylabArray/SkylabArray.js
+++ public/js/SkylabArray/SkylabArray.js
@@ -4,6 +4,16 @@ class SkylabArray {
 
   constructor(...args) {
     this.array = args;
+    this.length = this.lengthProperty(this.array);
   }
+
+  lengthProperty = (array) => {
+    const arrayLength = array;
+    let countElements = 0;
+    for (const element of arrayLength) {
+      countElements++;
+    }
+    return countElements;
+  };
 }
 export default SkylabArray;
