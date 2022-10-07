6caac805adaf4a9935682c9c374010b8c14bbcc6
Jordi Font <jordifv90@gmail.com>
2022-10-07 07:39:48 +0200

    Fix test to create array and use the lengh property of that class.

--- public/js/SkylabArray/SkylabArray.test.js
+++ public/js/SkylabArray/SkylabArray.test.js
@@ -1,12 +1,12 @@
-import lengthProperty from "./SkylabArray";
+import SkylabArray from "./SkylabArray";
 
 describe("Given the function SkylabArray", () => {
   describe("When it receives the values '1, 2, 3, 4, 9, 4, 9'", () => {
     test("It should return '7'", () => {
-      const values = "1, 2, 3, 4, 9, 4, 9";
-      const expectedLength = 4;
+      const values = new SkylabArray(1, 2, 3, 4, 9, 4, 9);
+      const expectedLength = 7;
 
-      const lengthValue = new lengthProperty(values);
+      const lengthValue = values.lengthProperty();
 
       expect(lengthValue).toBe(expectedLength);
     });
